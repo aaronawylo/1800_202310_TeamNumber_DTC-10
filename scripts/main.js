56c146f2adf9f442a7ac67b590faa3ebc7945e08
Derek Tran <dtran110@my.bcit.ca>
2023-03-08 00:17:29 -0800

    Change displayed name in main, adjust edit button

--- scripts/main.js
+++ scripts/main.js
@@ -5,12 +5,20 @@ function getNameFromAuth() {
             // Do something for the currently logged-in user here: 
             console.log(user.uid); //print the uid in the browser console
             console.log(user.displayName);  //print the user name in the browser console
-            user_Name = user.displayName;
+            currentUser = db.collection("users").doc(user.uid).get().then(
+                userDoc => {
+                    //get the data fields of the user
+                    var user_Name = userDoc.data().name;
+                    $("#name-goes-here").text(user_Name);
+                
+                })
+            
+
 
             //method #1:  insert with JS
             // document.getElementById("name-goes-here").innerText = user_Name;    
             //method #2:  insert using jquery
-            $("#name-goes-here").text(user_Name); //using jquery
+             //using jquery
             //method #3:  insert using querySelector
             // document.querySelector("#name-goes-here").innerText = user_Name
 
