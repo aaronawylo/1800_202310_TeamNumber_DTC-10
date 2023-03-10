d33d58431db24c6cd136c3a2364d106b3aa68ee3
Lulu Dong <luluberry8@gmail.com>
2023-03-10 13:20:44 -0800

    modify profilepage

--- scripts/health.js
+++ scripts/health.js
@@ -1,6 +1,6 @@
 function displayCardsDynamically(collection) {
   let cardTemplate = document.getElementById("healthCardTemplate");
-
+  
   db.collection(collection)
     .get() //the collection called "hikes"
     .then((allHealth) => {
@@ -10,19 +10,16 @@ function displayCardsDynamically(collection) {
         var title = doc.data().name; // get value of the "name" key
         var paragraph = doc.data().paragraph; // get value of the "paragraph" key
         var link = doc.data().url;
-  
         // var hikeCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
         // var website = doc.data().website; //gets the website field
         // var docID = doc.id;                //gets the document ID
         let newcard = cardTemplate.content.cloneNode(true);
-
         //update title and text and image
         newcard.querySelector(".card-title").innerHTML = title;
         // newcard.querySelector('.card-website').innerHTML = website
         newcard.querySelector(".card-text").innerHTML = paragraph;
         // newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
         newcard.querySelector(".view-article-button").href = link;
-
         //Optional: give unique ids to all elements for future use
         // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
         // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
@@ -30,10 +27,8 @@ function displayCardsDynamically(collection) {
 
         //attach to gallery, Example: "hikes-go-here"
         document.getElementById(collection + "-go-here").appendChild(newcard);
-
         //i++;   //Optional: iterate variable to serve as unique ID
       });
     });
 }
-
 displayCardsDynamically("health"); //input param is the name of the collection
