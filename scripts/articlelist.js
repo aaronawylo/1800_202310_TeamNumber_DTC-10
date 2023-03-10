fc831113d1900e19d513f31adc31b0eb4ff02c05
Aaron Lo <alo76@my.bcit.ca>
2023-03-10 13:50:47 -0800

    commit single page article list function

---
+++ scripts/articlelist.js
@@ -0,0 +1,46 @@
+function get_database_from_URL() {
+    console.log("Calling function")
+    const urlParams = new URLSearchParams(window.location.search);
+    var category = urlParams.get('category');
+    console.log(category);
+    displayCardsDynamically(category);
+}
+
+get_database_from_URL();
+
+function displayCardsDynamically(collection) {
+    let cardTemplate = document.getElementById("articleCardTemplate");
+
+    db.collection(collection).get()   //the collection
+        .then(allArticle => {
+            //var i = 1;  //Optional: if you want to have a unique ID for each hike
+            allArticle.forEach(doc => { //iterate thru each doc
+                var title = doc.data().name;       // get value of the "name" key
+                var paragraph = doc.data().paragraph;  // get value of the "paragraph" key
+                var website = doc.data().website
+                // var hikeCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
+                // var website = doc.data().website; //gets the website field
+                // var docID = doc.id;                //gets the document ID
+                let newcard = cardTemplate.content.cloneNode(true);
+
+                //update title and text and image
+                newcard.querySelector('.card-title').innerHTML = title;
+                // newcard.querySelector('.card-website').innerHTML = website
+                newcard.querySelector('.card-text').innerHTML = paragraph;
+                // newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+                newcard.querySelector('.view-article-button').href = website;
+
+                //Optional: give unique ids to all elements for future use
+                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+                //attach to gallery, Example: "hikes-go-here"
+                document.getElementById("article-go-here").appendChild(newcard);
+
+                //i++;   //Optional: iterate variable to serve as unique ID
+            })
+        })
+}
+
+  //input param is the name of the collection
--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -2,8 +2,34 @@
 // This function loads the parts of your skeleton 
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
-function loadSkeleton(){
-    console.log($('#navbarPlaceholder').load('./text/nav.html'));
+function loadSkeleton() {
+    console.log($('#navbarPlaceholder').load('./text/nav.html', activateDropdown)); 
     console.log($('#footerPlaceholder').load('./text/footer.html'));
 }
 loadSkeleton();  //invoke the function
+
+// Define a function to activate the dropdown menu
+function activateDropdown() {
+    // $('.dropdown-toggle').dropdown();
+    $('.dropdown-item').click(function () {
+        console.log($(this).text());
+        window.location.href = "./articlelist.html?category=" + $(this).text().toLowerCase();
+    });
+}
+
+activateDropdown();
+
+// function to add event listener to menu items
+// function addMenuListener(category) {
+//     console.log(category);
+//     document.getElementById("health").addEventListener("click", () => {
+//         passValueURL(category);
+//     })
+// }
+
+// function passValueURL(category) {
+//     console.log("clicked ..." + category);
+//     window.location.href = "./articlelist.html&category=" + category;
+// }
+
+// addMenuListener("health");
