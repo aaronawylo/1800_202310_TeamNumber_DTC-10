d0a42a4c0941d73d802ec1cc36351c48c47a1bb1
Derek Tran <dtran110@my.bcit.ca>
2023-03-14 14:14:13 -0700

    Fix name for review page when filtering by weather

--- scripts/review.js
+++ scripts/review.js
@@ -2,17 +2,37 @@ var collection = localStorage.getItem("category")
 console.log(collection)
 
 const urlParams = new URLSearchParams(window.location.search);
-    var ID = urlParams.get('docID');
-    console.log(ID);
+var ID = urlParams.get('docID');
+console.log(ID);
 
 function getArticleName(id) {
-    db.collection(collection)
+    db.collection("supplies")
         .doc(id)
         .get()
         .then((thisArticle) => {
             var articleName = thisArticle.data().name;
             document.getElementById("article-title").innerHTML = articleName;
-        });}
+        });
+
+    db.collection("finance")
+        .doc(id)
+        .get()
+        .then((thisArticle) => {
+            var articleName = thisArticle.data().name;
+            document.getElementById("article-title").innerHTML = articleName;
+        });
+
+    db.collection("health")
+        .doc(id)
+        .get()
+        .then((thisArticle) => {
+            var articleName = thisArticle.data().name;
+            document.getElementById("article-title").innerHTML = articleName;
+        });
+
+}
+
+
 
 getArticleName(ID);
 
