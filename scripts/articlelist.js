c958b6dfbf812ffc1ae1cb672f7bc24be2b199c8
Derek Tran <dtran110@my.bcit.ca>
2023-03-14 13:18:04 -0700

    Filter articles by chosen weather type

--- scripts/articlelist.js
+++ scripts/articlelist.js
@@ -11,42 +11,146 @@ get_database_from_URL();
 
 function displayCardsDynamically(collection) {
     let cardTemplate = document.getElementById("articleCardTemplate");
+    if (collection == "supplies" || collection == "finance" || collection == "health") {
+        db.collection(collection).get()   //the collection
+            .then(allArticle => {
+                allArticle.forEach(doc => { //iterate thru each doc
+                    var title = doc.data().name;       // get value of the "name" key
+                    var paragraph = doc.data().paragraph;  // get value of the "paragraph" key
+                    var website = doc.data().url
+                    var docID = doc.id;                //gets the document ID
+                    let newcard = cardTemplate.content.cloneNode(true);
+                    console.log(doc.data().tags)
+                    //update title and text and image
+                    newcard.querySelector('.card-title').innerHTML = title;
+                    // newcard.querySelector('.card-website').innerHTML = website
+                    newcard.querySelector('.card-text').innerHTML = paragraph;
+                    // newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+                    newcard.querySelector('.view-article-button').href = website;
 
-    db.collection(collection).get()   //the collection
-        .then(allArticle => {
-            //var i = 1;  //Optional: if you want to have a unique ID for each hike
-            allArticle.forEach(doc => { //iterate thru each doc
-                var title = doc.data().name;       // get value of the "name" key
-                var paragraph = doc.data().paragraph;  // get value of the "paragraph" key
-                var website = doc.data().url
-                // var hikeCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
-                // var website = doc.data().website; //gets the website field
-                var docID = doc.id;                //gets the document ID
-                let newcard = cardTemplate.content.cloneNode(true);
+                    newcard.querySelector('.review-article-button').href = "review.html?docID=" + docID;
 
-                //update title and text and image
-                newcard.querySelector('.card-title').innerHTML = title;
-                // newcard.querySelector('.card-website').innerHTML = website
-                newcard.querySelector('.card-text').innerHTML = paragraph;
-                // newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
-                newcard.querySelector('.view-article-button').href = website;
-                
-                newcard.querySelector('.review-article-button').href = "review.html?docID=" +docID;
-                
-            
+                    //Optional: give unique ids to all elements for future use
+                    // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                    // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                    // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
 
+                    //attach to gallery, Example: "hikes-go-here"
+                    document.getElementById("article-go-here").appendChild(newcard);
 
-                //Optional: give unique ids to all elements for future use
-                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
-                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
-                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
-
-                //attach to gallery, Example: "hikes-go-here"
-                document.getElementById("article-go-here").appendChild(newcard);
-
-                //i++;   //Optional: iterate variable to serve as unique ID
+                    //i++;   //Optional: iterate variable to serve as unique ID
+                })
             })
-        })
+    }
+    else {
+        db.collection("supplies").get()   //the collection
+            .then(allArticle => {
+                allArticle.forEach(doc => { //iterate thru each doc
+
+                    if (doc.data().tags.includes(collection)) {
+
+                        var title = doc.data().name;       // get value of the "name" key
+                        var paragraph = doc.data().paragraph;  // get value of the "paragraph" key
+                        var website = doc.data().url
+                        var docID = doc.id;                //gets the document ID
+                        let newcard = cardTemplate.content.cloneNode(true);
+                        console.log(doc.data().tags)
+                        //update title and text and image
+                        newcard.querySelector('.card-title').innerHTML = title;
+                        // newcard.querySelector('.card-website').innerHTML = website
+                        newcard.querySelector('.card-text').innerHTML = paragraph;
+                        // newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+                        newcard.querySelector('.view-article-button').href = website;
+
+                        newcard.querySelector('.review-article-button').href = "review.html?docID=" + docID;
+
+                        //Optional: give unique ids to all elements for future use
+                        // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                        // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                        // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+                        //attach to gallery, Example: "hikes-go-here"
+                        document.getElementById("article-go-here").appendChild(newcard);
+
+                        //i++;   //Optional: iterate variable to serve as unique ID
+                    }
+
+                })
+            })
+
+
+            db.collection("health").get()   //the collection
+            .then(allArticle => {
+                allArticle.forEach(doc => { //iterate thru each doc
+
+                    if (doc.data().tags.includes(collection)) {
+
+                        var title = doc.data().name;       // get value of the "name" key
+                        var paragraph = doc.data().paragraph;  // get value of the "paragraph" key
+                        var website = doc.data().url
+                        var docID = doc.id;                //gets the document ID
+                        let newcard = cardTemplate.content.cloneNode(true);
+                        console.log(doc.data().tags)
+                        //update title and text and image
+                        newcard.querySelector('.card-title').innerHTML = title;
+                        // newcard.querySelector('.card-website').innerHTML = website
+                        newcard.querySelector('.card-text').innerHTML = paragraph;
+                        // newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+                        newcard.querySelector('.view-article-button').href = website;
+
+                        newcard.querySelector('.review-article-button').href = "review.html?docID=" + docID;
+
+                        //Optional: give unique ids to all elements for future use
+                        // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                        // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                        // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+                        //attach to gallery, Example: "hikes-go-here"
+                        document.getElementById("article-go-here").appendChild(newcard);
+
+                        //i++;   //Optional: iterate variable to serve as unique ID
+                    }
+
+                })
+            })
+
+            db.collection("finance").get()   //the collection
+            .then(allArticle => {
+                allArticle.forEach(doc => { //iterate thru each doc
+
+                    if (doc.data().tag.includes(collection)) {
+
+                        var title = doc.data().name;       // get value of the "name" key
+                        var paragraph = doc.data().paragraph;  // get value of the "paragraph" key
+                        var website = doc.data().url
+                        var docID = doc.id;                //gets the document ID
+                        let newcard = cardTemplate.content.cloneNode(true);
+                        console.log(doc.data().tag)
+                        //update title and text and image
+                        newcard.querySelector('.card-title').innerHTML = title;
+                        // newcard.querySelector('.card-website').innerHTML = website
+                        newcard.querySelector('.card-text').innerHTML = paragraph;
+                        // newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+                        newcard.querySelector('.view-article-button').href = website;
+
+                        newcard.querySelector('.review-article-button').href = "review.html?docID=" + docID;
+
+                        //Optional: give unique ids to all elements for future use
+                        // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                        // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                        // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+                        //attach to gallery, Example: "hikes-go-here"
+                        document.getElementById("article-go-here").appendChild(newcard);
+
+                        //i++;   //Optional: iterate variable to serve as unique ID
+                    }
+
+                })
+            })
+
+    }
+
 }
 
   //input param is the name of the collection
